0849040ca7b2dff21c49522e0e09afb7bcc57bfc
Alex Halyshev <alexander.halyshev@gmail.com>
2024-10-23 23:42:44 +0300

    task3 well done

--- task3/task3.sh
+++ task3/task3.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 for num in {1..20}; do
-	touch $num.txt;
+	mkdir ./$num #создаем папки
+	echo $num.txt > ./$num/$num.txt #создаем файлы с записью их имени в содержимое
+	ln -s ./$num/$num.txt ./$num.txt #создаем симлинки
 done
 for num in {1..20}; do
 	if [[ $num != *4* ]]; then
-		rm $num.txt
+		rm -r ./$num #не мусорим, удалим файлы вместе с папками
+		rm ./$num.txt #симлинки туда же
+	elif [[ $num == 14 ]]; then
+		echo "Vim используют те, кто не может с него выйти :D" > ./$num.txt #пишем гадости в 14.txt, сократим путь через симлинк
 	fi
 done
