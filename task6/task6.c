f2580d56015d88d61777eea0d590918c28065341
Alex Halyshev <alexander.halyshev@gmail.com>
2024-11-05 23:08:36 +0300

    strukture test

--- task6/task6.c
+++ task6/task6.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <math.h>
-
 #define NUM 2
 
+int flag = 1;
+
 int main(){
-    while (true)
+    int n;
+    while (flag == 1)
     {
-        printf("o kurwa");
+        printf("Введите натуральное число: ");
+        scanf("%d", &n);
+        if (n > 0) {
+           printf("molodec\n");
+           flag=0;
+        }
+        else {
+            printf("uchi matematiku \n");
+        }
     }
-    
+    return(0);
 }
